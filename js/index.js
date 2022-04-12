cd69555f094050ff7e8d19f14d437a18cab0b4f3
Jessica Cintra <jessica.cintra0712@gmail.com>
2022-04-12 13:40:15 -0300

    js testing

---
+++ js/index.js
@@ -0,0 +1,44 @@
+const personalKey="9rk8G4OlOHKHWxEO3PgtkuXh5Iqy0Lf75zKEqbLZ";
+let media =$("#media").hide()
+let time=null;
+let onChange=false;
+$(document).ready( function() {
+    let now = new Date();
+    let month = (now.getMonth() + 1);               
+    let day = now.getDate();
+    if (month < 10) 
+        month = "0" + month;
+    if (day < 10) 
+        day = "0" + day;
+    let today = now.getFullYear() + '-' + month + '-' + day;
+    let  dataDeHoje=$('#data').val(today).toString();
+    console.log(dataDeHoje)
+    // $.ajax({
+    //     url: `https://api.nasa.gov/planetary/apod?api_key=${personalKey}&date=${dataDeHoje}`,
+    //     success: function (resposta) {
+    //         $("#media").show()  
+    //       $("#media").attr("src", resposta.url)
+    //     },
+    //   });
+});
+
+
+$("#data").change(function(){
+    onChange=true;
+    clearTimeout(time);
+    time=setTimeout(function(){
+        onChange=false;
+        dataUsuario=$("#data").val()
+        $.ajax({
+            url: `https://api.nasa.gov/planetary/apod?api_key=${personalKey}&date=${dataUsuario}`,
+            success: function (resposta) {
+                $("#media").show()  
+              $("#media").attr("src", resposta.url)
+            },
+          });
+    },1000)
+})
+
+let dataDeHoje
+
+
