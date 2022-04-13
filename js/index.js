1532f889b284898953b7ee3e1dfea4ac4eee6199
Jessica Cintra <jessica.cintra0712@gmail.com>
2022-04-12 21:25:56 -0300

    animations

--- js/index.js
+++ js/index.js
@@ -3,30 +3,34 @@ let media =$("#media-img").hide()
 let mediaVideo =$("#media-video").hide()
 let time=null;
 let onChange=false;
-// $(document).ready( function() {
-//     let now = new Date();
-//     let month = (now.getMonth() + 1);               
-//     let day = now.getDate();
-//     if (month < 10) 
-//         month = "0" + month;
-//     if (day < 10) 
-//         day = "0" + day;
-//     let today = now.getFullYear() + '-' + month + '-' + day;
-//     let  dataDeHoje=$('#data').val(today).toString();
-//     $("#data").autofocus=true;
-//     console.log(dataDeHoje)
-//     // $.ajax({
-//     //     url: `https://api.nasa.gov/planetary/apod?api_key=${personalKey}&date=${dataDeHoje}`,
-//     //     success: function (resposta) {
-//     //         $("#media").show()  
-//     //       $("#media").attr("src", resposta.url)
-//     //     },
-//     //   });
-// });
-// document.querySelector("#data").addEventListener("focus",function(){
-//     document.querySelector("#data").style.backgroundColor="#1DA400"
-//     document.querySelector("#data"),style.color="white"
-// });
+$(document).ready( function() {
+    let now = new Date();
+    let month = (now.getMonth() + 1);               
+    let day = now.getDate();
+    if (month < 10) 
+        month = "0" + month;
+    if (day < 10) 
+        day = "0" + day;
+    let today = now.getFullYear() + '-' + month + '-' + day;
+    console.log(today)
+    let  dataDeHoje=$('#data').val(today).toString();
+    $.ajax({
+        url: `https://api.nasa.gov/planetary/apod?api_key=${personalKey}&date=${today}`,
+        success: function (resposta) {
+            if(resposta.media_type=="image"){
+                if($("#media-img").show()){
+                    $("#media-video").hide() 
+                }  
+                $("#media-img").attr("src", resposta.url)
+            }else if(resposta.media_type=="video"){
+                if($("#media-video").show()){
+                    $("#media-img").hide()
+                }  
+                $("#media-video").attr("src", resposta.url)
+            }
+        },
+      });
+});
 
 $("#data").change(function(){
     onChange=true;
